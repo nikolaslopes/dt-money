00d6552886b330a2d7ae3d8c7369a234a21c8611
nikolaslopes <nikolaslopes.dev@gmail.com>
2021-10-15 17:09:17 -0300

    :tada: feature: api connection

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createServer, Model } from 'miragejs';
+
 import { App } from './App';
 
-import { createServer } from 'miragejs';
-
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Little hide',
+          type: 'withdraw',
+          category: 'Food',
+          amount: 2000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Little hideeee',
+          type: 'withdraw',
+          category: 'Food',
+          amount: 222000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        }
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
   }
-})
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
